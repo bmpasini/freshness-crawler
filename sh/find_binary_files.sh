6ef8cc49a1f58f6def884e6ac175a5d45ed8c938
bmpasini <bmpasini@gmail.com>
2015-07-28 00:28:01 -0400

    expand readme

--- sh/find_binary_files.sh
+++ sh/find_binary_files.sh
@@ -1,1 +1,3 @@
-print (-B $_ ? "$_\n" : "" ) for @ARGV
+find file_tmp/ -type f -exec perl -e 'print (-B $_ ? "$_\n" : "" ) for @ARGV' {} + > binary.txt &
+# xargs rm < binary.txt
+# xargs rm < find file_tmp/ -type f -exec perl -e 'print (-B $_ ? "$_\n" : "" ) for @ARGV' {} + &
